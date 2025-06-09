6fcaf4893b82c32d43800ef079e8e034b4d737b8
Mark <43383309+markthr@users.noreply.github.com>
2025-06-09 19:18:12 -0400

    initial pybind setup
    Using pybind's built in translation between C++ types and Python types results in the bound transforms not passing their output to Python because input values are passed by copy.
    
    To pass by reference, a consistent type must be used and it makes much more sense to have the C++ interface with python types than to have Python interface with C++ types given that relatively seamless interfacing with numpy is desired.
    
    numpy arrays cannot be resized which requires either a translation layer that then call the transforms which are built on resizable vectors or refactoring the transforms to use std::arrays instead of vectors.

--- lib/libc/transforms/transforms.cpp
+++ lib/libc/transforms/transforms.cpp
@@ -1,5 +1,14 @@
 #include "transforms.hpp"
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
-// all implementation for the templates is in the header files. Using this file to link project with CMake
-// not very elegant but it is currently working
+namespace py = pybind11;
+
+PYBIND11_MODULE(transforms, m) {
+    m.doc() = "pybind11 example";
+
+    py::class_<DCT_2<double, 8>>(m, "dct_8")
+        .def(py::init<>())
+        .def("transform", &DCT_2<double, 8>::transform)
+        .def("inverse", &DCT_2<double, 8>::inverse);
+}
