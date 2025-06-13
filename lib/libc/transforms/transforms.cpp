a8fa1b148aad2c7f78ea7ef67bb284b4f36ae7a5
Mark <43383309+markthr@users.noreply.github.com>
2025-06-13 15:32:04 -0400

    Python binding fix
    The python bindings now work and can be used to properly perform the transforms within python. For now, only the DCT_2 has been bound.
    
    Using a C++ span for reading numpy data without needing to copy it worked. There is currently no support for writing the output directly to a provided buffer and currently no type checks are performed on top of those that pybind11 performs.
    
    Current idea for handling python bindings is to create an inheriting class for each transform class that adds functions which takes numpy arrays as input and delegate to the parent class (the transform) to avoid having any pybind code mixed in with code that implements transforms.

--- lib/libc/transforms/transforms.cpp
+++ lib/libc/transforms/transforms.cpp
@@ -1,8 +1,23 @@
 #include "transforms.hpp"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <pybind11/numpy.h>
 
 namespace py = pybind11;
+
+class Py_DCT : public DCT_2<double> {
+    public:
+        using DCT_2<double>::DCT_2;
+
+        std::vector<double> py_transform(py::array_t<double, py::array::c_style>& in) {
+            return this->transform(std::span(in.data(), in.size()));
+        }
+
+        std::vector<double> py_inverse(py::array_t<double, py::array::c_style>& in) {
+            return this->inverse(std::span(in.data(), in.size()));
+        }
+};
+
 
 PYBIND11_MODULE(transforms, m) {
     m.doc() = "pybind11 example";
@@ -10,14 +25,24 @@ PYBIND11_MODULE(transforms, m) {
     //TODO: pybind11 does not understand how to convert python arrays to spans, current code is a placeholder
     // there are forks of pybind11 that give span a default converter along with python libraries that port
     // C++ spans into python. Relying on more libraries seems not worthwhile in this case though.
-    py::class_<DCT_2<double>>(m, "dct_2")
+
+    /**
+     * code pasted into the terminal for testing
+    python - << 'EOF'
+    import numpy as np
+    import transforms as tm
+    dct = tm.dct_2(8)
+    res = dct.transform(np.array([1, 2, 3, 4, 5, 6, 7, 8]))
+    print(res)
+    EOF
+     *
+    Exptected Results: [12.727922061357855, -6.442323022705137, 0.0, -0.6734548009039405, 0.0, -0.20090290373599626, 0.0, -0.05070232275964579]
+     */
+    
+    py::class_<Py_DCT>(m, "dct_2")
         .def(py::init<int>())
         .def("transform",
-            static_cast<std::vector<double> (DCT_2<double>::*) (std::span<const double>)>(
-                &DCT_2<double>::transform)
-        )
+            &Py_DCT::py_transform)
         .def("inverse",
-            static_cast<std::vector<double> (DCT_2<double>::*) (std::span<const double>)>(
-                &DCT_2<double>::inverse)
-        );
+            &Py_DCT::py_inverse);
 }
